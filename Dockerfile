a6e44b426e4eb35387510d4f61ea6538d2aeb9a7
teamplayer3 <alex.teamplayer@gmail.com>
2023-12-01 17:40:44 +0100

    updated

--- Dockerfile
+++ Dockerfile
@@ -4,21 +4,18 @@ SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 RUN mkdir data
 WORKDIR /data
 
+COPY requirements.txt requirements.txt
+
 # Install Python dependencies (Worker Template)
 RUN pip install --upgrade pip && \
-    pip install safetensors==0.3.1 sentencepiece huggingface_hub \
-        git+https://github.com/winglian/runpod-python.git@fix-generator-check ninja==1.11.1
-RUN git clone https://github.com/turboderp/exllama
-RUN pip install -r exllama/requirements.txt
+    pip install -r requirements.txt
 
 COPY handler.py /data/handler.py
 COPY __init.py__ /data/__init__.py
 
-ENV PYTHONPATH=/data/exllama
 ENV MODEL_REPO=""
 ENV PROMPT_PREFIX=""
 ENV PROMPT_SUFFIX=""
-ENV HUGGINGFACE_HUB_CACHE="/runpod-volume/huggingface-cache/hub"
 ENV TRANSFORMERS_CACHE="/runpod-volume/huggingface-cache/hub"
 
 CMD [ "python", "-m", "handler" ]
--- handler.py
+++ handler.py
@@ -1,122 +1,81 @@
-from model import ExLlama, ExLlamaCache, ExLlamaConfig
-from tokenizer import ExLlamaTokenizer
-from generator import ExLlamaGenerator
-import os, glob
+import os
 import logging
-from typing import Generator, Union
 import runpod
-from huggingface_hub import snapshot_download
-from copy import copy
 
-import re
-import codecs
+from transformers import pipeline
 
-ESCAPE_SEQUENCE_RE = re.compile(r'''
-    ( \\U........      # 8-digit hex escapes
-    | \\u....          # 4-digit hex escapes
-    | \\x..            # 2-digit hex escapes
-    | \\[0-7]{1,3}     # Octal escapes
-    | \\N\{[^}]+\}     # Unicode characters by name
-    | \\[\\'"abfnrtv]  # Single-character escapes
-    )''', re.UNICODE | re.VERBOSE)
 
-def decode_escapes(s):
-    def decode_match(match):
-        return codecs.decode(match.group(0), 'unicode-escape')
+pipe = None
 
-    return ESCAPE_SEQUENCE_RE.sub(decode_match, s)
 
-logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
+def inference(event) -> [str]:
+    global pipe
 
-def load_model():
-    global generator, default_settings
-
-    if not generator:
-        model_directory = snapshot_download(repo_id=os.environ["MODEL_REPO"], revision=os.getenv("MODEL_REVISION", "main"))
-        tokenizer_path = os.path.join(model_directory, "tokenizer.model")
-        model_config_path = os.path.join(model_directory, "config.json")
-        st_pattern = os.path.join(model_directory, "*.safetensors")
-        st_files = glob.glob(st_pattern)
-        if not st_files:
-            raise ValueError(f"No safetensors files found in {model_directory}")
-        model_path = st_files[0]
-
-        # Create config, model, tokenizer and generator
-        config = ExLlamaConfig(model_config_path)               # create config from config.json
-        config.model_path = model_path                          # supply path to model weights file
-
-        gpu_split = os.getenv("GPU_SPLIT", "")
-        if gpu_split:
-            config.set_auto_map(gpu_split)
-            config.gpu_peer_fix = True
-        alpha_value = int(os.getenv("ALPHA_VALUE", "1"))
-        config.max_seq_len = int(os.getenv("MAX_SEQ_LEN", "2048"))
-        if alpha_value != 1:
-            config.alpha_value = alpha_value
-            config.calculate_rotary_embedding_base()
-
-        model = ExLlama(config)                                 # create ExLlama instance and load the weights
-        tokenizer = ExLlamaTokenizer(tokenizer_path)            # create tokenizer from tokenizer model file
-
-        cache = ExLlamaCache(model)                             # create cache for inference
-        generator = ExLlamaGenerator(model, tokenizer, cache)   # create generator
-        default_settings = {
-            k: getattr(generator.settings, k) for k in dir(generator.settings) if k[:2] != '__'
-        }
-    return generator, default_settings
-
-generator = None
-default_settings = None
-prompt_prefix = decode_escapes(os.getenv("PROMPT_PREFIX", ""))
-prompt_suffix = decode_escapes(os.getenv("PROMPT_SUFFIX", ""))
-
-def generate_with_streaming(prompt, max_new_tokens):
-    global generator
-    generator.end_beam_search()
-
-    # Tokenizing the input
-    ids = generator.tokenizer.encode(prompt)
-    ids = ids[:, -generator.model.config.max_seq_len:]
-
-    generator.gen_begin_reuse(ids)
-    initial_len = generator.sequence[0].shape[0]
-    has_leading_space = False
-    for i in range(max_new_tokens):
-        token = generator.gen_single_token()
-        if i == 0 and generator.tokenizer.tokenizer.IdToPiece(int(token)).startswith('▁'):
-            has_leading_space = True
-
-        decoded_text = generator.tokenizer.decode(generator.sequence[0][initial_len:])
-        if has_leading_space:
-            decoded_text = ' ' + decoded_text
-
-        yield decoded_text
-        if token.item() == generator.tokenizer.eos_token_id:
-            break
-
-def inference(event) -> Union[str, Generator[str, None, None]]:
     logging.info(event)
     job_input = event["input"]
     if not job_input:
         raise ValueError("No input provided")
 
-    prompt: str = job_input.pop("prompt_prefix", prompt_prefix) + job_input.pop("prompt") + job_input.pop("prompt_suffix", prompt_suffix)
+    prompt: str = job_input.pop("prompt")
+    temperature: float = job_input.pop("temperature", 1.0)
     max_new_tokens = job_input.pop("max_new_tokens", 100)
-    stream: bool = job_input.pop("stream", False)
+    do_sample = job_input.pop("do_sample", False)
+    top_p = job_input.pop("top_p", 0.95)
+    repetition_penalty = job_input.pop("repetition_penalty", 1.0)
+    n = job_input.pop("n", 1)
 
-    generator, default_settings = load_model()
+    output = []
 
-    settings = copy(default_settings)
-    settings.update(job_input)
-    for key, value in settings.items():
-        setattr(generator.settings, key, value)
+    for i in range(n):
+        output_text = pipe(prompt, temperature=temperature,
+                           max_new_tokens=max_new_tokens, do_sample=do_sample, top_p=top_p, repetition_penalty=repetition_penalty)[0]["generated_text"]
 
-    if stream:
-        output: Union[str, Generator[str, None, None]] = generate_with_streaming(prompt, max_new_tokens)
-        for res in output:
-            yield res
+        logging.debug(f"generated sample {i}/{n}: {output_text}")
+        output.append(output_text)
+
+    yield output
+
+
+def load_model():
+    revision = os.getenv("MODEL_REVISION", "main")
+    model = os.environ["MODEL_REPO"]
+    quantization = os.getenv("MODEL_QUANTIZATION", None)
+
+    from transformers import AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model, use_fast=True)
+
+    if quantization == "GPTQ":
+        from auto_gptq import AutoGPTQForCausalLM
+
+        model = AutoGPTQForCausalLM.from_quantized(model,
+                                                   use_safetensors=True,
+                                                   trust_remote_code=True,
+                                                   device="cuda:0",
+                                                   quantize_config=None,
+                                                   revision=revision,
+                                                   cache_dir=os.environ["MODEL_CACHE_DIR"])
     else:
-        output_text = generator.generate_simple(prompt, max_new_tokens = max_new_tokens)
-        yield output_text[len(prompt):]
+        from transformers import AutoModelForCausalLM
 
-runpod.serverless.start({"handler": inference})
+        model = AutoModelForCausalLM.from_pretrained(
+            model, revision=revision, cache_dir=os.environ["MODEL_CACHE_DIR"], device="cuda:0", trust_remote_code=True)
+
+    pipe = pipeline("text-generation", model=model,
+                    tokenizer=tokenizer, trust_remote_code=True)
+
+    return pipe
+
+
+def main():
+    global pipe
+
+    pipe = load_model()
+
+    runpod.serverless.start(
+        {"handler": inference, "return_aggregate_stream": True})
+
+
+if __name__ == "__main__":
+    main()
